5fadca07bf0bdd1759161dfee9a06146346b900e
Johann Bogota <johann.bogota@mail.escuelaing.edu.co>
2021-03-22 20:42:42 -0500

    Parte 1

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,75 +1,78 @@
 var app = (function () {
-
-    class Point{
-        constructor(x,y){
-            this.x=x;
-            this.y=y;
-        }        
+  class Point {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
     }
-    
-    var stompClient = null;
+  }
 
-    var addPointToCanvas = function (point) {        
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
-        ctx.stroke();
-    };
-    
-    
-    var getMousePosition = function (evt) {
-        canvas = document.getElementById("canvas");
-        var rect = canvas.getBoundingClientRect();
-        return {
-            x: evt.clientX - rect.left,
-            y: evt.clientY - rect.top
-        };
-    };
+  var stompClient = null;
 
+  var addPointToCanvas = function (point) {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
+    ctx.stroke();
+  };
 
-    var connectAndSubscribe = function () {
-        console.info('Connecting to WS...');
-        var socket = new SockJS('/stompendpoint');
-        stompClient = Stomp.over(socket);
-        
-        //subscribe to /topic/TOPICXX when connections succeed
-        stompClient.connect({}, function (frame) {
-            console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
-            });
-        });
+  var publicPoint = function (point) {
+    stompClient.send("/topic/newpoint", {}, JSON.stringify(point));
+  };
 
-    };
-    
-    
-
+  var getMousePosition = function (evt) {
+    canvas = document.getElementById("canvas");
+    var rect = canvas.getBoundingClientRect();
     return {
-
-        init: function () {
-            var can = document.getElementById("canvas");
-            
-            //websocket connection
-            connectAndSubscribe();
-        },
-
-        publishPoint: function(px,py){
-            var pt=new Point(px,py);
-            console.info("publishing point at "+pt);
-            addPointToCanvas(pt);
-
-            //publicar el evento
-        },
-
-        disconnect: function () {
-            if (stompClient !== null) {
-                stompClient.disconnect();
-            }
-            setConnected(false);
-            console.log("Disconnected");
-        }
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top,
     };
+  };
 
+  var connectAndSubscribe = function () {
+    console.info("Connecting to WS...");
+    var socket = new SockJS("/stompendpoint");
+    stompClient = Stomp.over(socket);
+
+    //subscribe to /topic/TOPICXX when connections succeed
+    stompClient.connect({}, function (frame) {
+      console.log("Connected: " + frame);
+
+      stompClient.subscribe("/topic/newpoint", function (message) {
+        var theObject = JSON.parse(message.body);
+        alert(theObject.x + "--" + theObject.y);
+        //console.log("--");
+        //console.log(eventbody.body);
+        //console.log(JSON.parse(eventbody.body).content);
+        //console.log("--");
+        //showGreeting(JSON.parse(eventbody.body).content);
+      });
+    });
+  };
+
+  return {
+    init: function () {
+      var can = document.getElementById("canvas");
+
+      //websocket connection
+      connectAndSubscribe();
+    },
+
+    publishPoint: function (px, py) {
+      var pt = new Point(px, py);
+      console.info("publishing point at " + pt);
+      addPointToCanvas(pt);
+
+      //publicar el evento
+      publicPoint(pt);
+    },
+
+    disconnect: function () {
+      if (stompClient !== null) {
+        stompClient.disconnect();
+      }
+      setConnected(false);
+      console.log("Disconnected");
+    },
+  };
 })();
